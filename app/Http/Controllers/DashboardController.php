fcd6121991c3aefaac18ee5cb7f72d17392581e6
Noah Gillard <noah.gillard@student.kdg.be>
2021-05-07 14:58:46 +0200

    layout wip

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,7 +7,12 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+class DashboardController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index() {
+
+        return view('dashboard');
+    }
 }
